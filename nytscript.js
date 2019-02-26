44af321f6e4506d2950ee2761767ae018e5ca0c1
TheRealJoeFriel <jfriel46@gmail.com>
2019-02-25 20:13:34 -0500

    Added NYT pictures

--- nytscript.js
+++ nytscript.js
@@ -1,3 +1,4 @@
+// Creates a connection to NYT API and returns a json object 
 function httpGetNYT() {
     var theUrl = "https://api.nytimes.com/svc/topstories/v2/science.json?api-key=x19FApjOENkQi6yvd235Ab30dS7WXyDJ";
     var xmlHttp = new XMLHttpRequest();
@@ -12,9 +13,11 @@ function getFirstFiveNYT(nyt) {
     for (i = 0; i < 5; i++) {
         var story = nyt["results"][i];
 
+        document.getElementById("nytstories").innerHTML += "<span>"
+        document.getElementById("nytstories").innerHTML += "<img src="+story.multimedia[0].url+" alt='Image Unable to Load' height=50px width=50px align='left'>";
         document.getElementById("nytstories").innerHTML += "<a href="+ story.short_url + '>' + story.title + '</a>';
-        document.getElementById("nytstories").innerHTML += '<br>';
-        document.getElementById("nytstories").innerHTML += "<div class=\'abstract col-med-offset-1'><ul><li>" + story.abstract + "</li></ul></div>";
+        document.getElementById("nytstories").innerHTML += "<div class=\'abstract col-med-offset-1'>" + story.abstract + "</div>";
+        document.getElementById("nytstories").innerHTML += "</span><br>"
 
     }
 }
--- weatherscript.js
+++ weatherscript.js
@@ -1,4 +1,4 @@
-
+// Creates a connection to AccuWeather API and returns a json object
 function httpGetWeather() {
     var theUrl = "https://dataservice.accuweather.com/currentconditions/v1/333562?apikey=eAEd8x5QoJ7YgOa9vGEYb6QN5B2Dknvo&language=en-us&details=false HTTP/1.1";
     var xmlHttp = new XMLHttpRequest();
@@ -7,6 +7,7 @@ function httpGetWeather() {
     return xmlHttp.responseText;
 }
 
+// Retrieves the local weather and weather icon
 function displayWeather(weather) {
     var impTemp = weather[0]["Temperature"]["Imperial"]["Value"] + "<sup>o</sup>" + weather[0]["Temperature"]["Imperial"]["Unit"];
     var metTemp = weather[0]["Temperature"]["Metric"]["Value"] + "<sup>o</sup>" + weather[0]["Temperature"]["Metric"]["Unit"];
